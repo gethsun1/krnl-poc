4687a6997ed4ac9a6508435a0ea7dde37a74ed2c
Gethsun 1 <gethsun09@mail.com>
2024-11-27 19:16:13 +0300

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div style={{ textAlign: 'center', color: '#011eff', padding: '2rem' }}>
+      <h1>KRNL PoC Demo</h1>
+      <p>Welcome to the KRNL Proof of Concept DApp!</p>
+    </div>
+  );
+};
+
+export default App;
